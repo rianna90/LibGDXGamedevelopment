4ad1b48e438ba5ffb53c591a2d1d2bf8ac4b7e8a
Rianne <r_overweg@msn.com>
2013-04-16 12:03:50 +0200

    commit

--- my-gdx-game/src/com/me/mygdxgame/MyGdxGame.java
+++ my-gdx-game/src/com/me/mygdxgame/MyGdxGame.java
@@ -1,47 +1,24 @@
 package com.me.mygdxgame;
 
-import java.util.Iterator;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
+
 
 public class MyGdxGame implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	
-	Texture dropImage;
-	Texture bucketImage;
-	Sound dropSound;
+	private Texture background;
+
 	Music rainMusic;
-	
-	Rectangle bucket;
-	Array<Rectangle> raindrops;
-	long lastDropTime;
-	
 	
 	@Override
 	public void create() {		
 		
-		dropImage = new Texture(Gdx.files.internal("drop.jpg"));
-	    bucketImage = new Texture(Gdx.files.internal("bucket.jpg"));
-	    
-	    dropSound = Gdx.audio.newSound(Gdx.files.internal("waterdrop.wav"));
+	    background = new Texture(Gdx.files.internal("background.png"));
 	    rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 	      
 	    rainMusic.setLooping(true);
@@ -50,80 +27,24 @@ public class MyGdxGame implements ApplicationListener {
 	    camera = new OrthographicCamera();
 	    camera.setToOrtho(false, 800, 480);
 	    batch = new SpriteBatch();
-	    
-	    bucket = new Rectangle();
-	    bucket.x = 800 / 2 - 48 / 2;
-	    bucket.y = 20;
-	    bucket.width = 48;
-	    bucket.height = 48;
-	    
-	    raindrops = new Array<Rectangle>();
-	    spawnRaindrop();
-
 	}
-	
-	private void spawnRaindrop() {
-	      Rectangle raindrop = new Rectangle();
-	      raindrop.x = MathUtils.random(0, 800-48);
-	      raindrop.y = 480;
-	      raindrop.width = 48;
-	      raindrop.height = 48;
-	      raindrops.add(raindrop);
-	      lastDropTime = TimeUtils.nanoTime();
-	   }
+
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		texture.dispose();
-	    dropImage.dispose();
-	    bucketImage.dispose();
-	    dropSound.dispose();
-	    rainMusic.dispose();
-
 	}
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-	    
 	    camera.update();
 		
 		batch.setProjectionMatrix(camera.combined);
 		
 		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-		      batch.draw(dropImage, raindrop.x, raindrop.y);
-		   }
+		batch.draw(background, 0, 0, 800, 600, 0, 0, 800, 600, false, false);
 		batch.end();
 		
-		if(Gdx.input.isTouched()) {
-		      Vector3 touchPos = new Vector3();
-		      touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-		      camera.unproject(touchPos);
-		      bucket.x = touchPos.x - 48 / 2;
-		   }
-		
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-	    if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-	    
-	    if(bucket.x < 0) bucket.x = 0;
-	    if(bucket.x > 800 - 48) bucket.x = 800 - 48;
-		
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-		
-		Iterator<Rectangle> iter = raindrops.iterator();
-	      while(iter.hasNext()) {
-	         Rectangle raindrop = iter.next();
-	         raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-	         if(raindrop.y + 48 < 0) iter.remove();
-	         if(raindrop.overlaps(bucket)) {
-	            dropSound.play();
-	            iter.remove();
-	         }
-	      }
 	}
 
 	@Override
