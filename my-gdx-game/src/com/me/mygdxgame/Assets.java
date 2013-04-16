435f21d40b370924d49685bc0b336caf2293da5c
Rianne <r_overweg@msn.com>
2013-04-16 13:42:05 +0200

    Assets.java

---
+++ my-gdx-game/src/com/me/mygdxgame/Assets.java
@@ -0,0 +1,21 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Assets {
+	public static Texture background;
+	
+	public static Texture loadTexture (String file){
+		return new Texture(Gdx.files.internal(file));
+	}
+		
+	public static void load(){
+		background = loadTexture("background.jpg");
+	}
+
+}
+
+
+
+
